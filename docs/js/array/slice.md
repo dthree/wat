6516d5ffe1ee31e22e8ad14ca58a1e83c1018332
dthree <threedeecee@gmail.com>
2015-08-12 22:41:37 -0700

    more

--- docs/js/array/slice.md
+++ docs/js/array/slice.md
@@ -1,5 +1,7 @@
-[].slice([begin[, end]])
+## [].slice([begin[, end]])
+
 ```js
 ['a', 'b', 'c'].slice(0, 2);   // ['a', 'b'];
 ['a', 'b', 'c'].slice(2);      // ['c']
-```
+```  
+             
---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+"use strict";
+
+var gulp = require("gulp");
+var eslint = require("gulp-eslint");
+var indexer = require('./lib/indexer');
+
+gulp.task("lint", function(){
+  return gulp.src(["lib/*.js", "./*.js", "./bin/*.js"])
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failOnError());
+});
+
+gulp.task("buildIndex", function(done) {
+	indexer.build(function(index){
+		indexer.write(index);
+	  console.log(index);
+	  done();
+	});
+});
+
+gulp.task("watch", function() {
+  gulp.watch("docs/**/*.*", ["buildIndex"]);
+});
+
+gulp.task("default", ["watch"]);
